e51d3ebbc2d9db29d06de861d53b8962454faafc
Marina Ma <mma79665@gmail.com>
2025-04-12 18:31:35 -0400

    modified sub and log functions in calculator

--- calculator.py
+++ calculator.py
@@ -21,7 +21,7 @@ def hypotenuse(a, b):
 def add(a, b):
     return a+b
 
-def sub(a, b):
+def subtract(a, b):
     return  a-b
 
 def mul(a, b):
@@ -33,7 +33,7 @@ def div(a,b):
     else:
         return a/b
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or b <= 1:
         raise ValueError("Domain Error")
     else:
